efbe6f394017e9595621ca5b6359facea0c316ec
Joaquin Metayer <joaquinmetayer@gmail.com>
2023-12-18 12:37:48 -0300

    change readme

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,3 @@ export default function RootLayout({
     </html>
   );
 }
-
-// add the next line if you want a background blur on hero
-// <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
