6120b13f75c7a5f4cee0fc8bfc8453f9740ac4a9
Joaquin Metayer <joaquinmetayer@gmail.com>
2024-01-03 21:14:28 -0300

    30

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
 "use client";
+import { ShaderGradient, ShaderGradientCanvas } from "shadergradient";
+import * as reactSpring from "@react-spring/three";
+import * as drei from "@react-three/drei";
+import * as fiber from "@react-three/fiber";
 
 export default function Home() {
   return (
     <div className="bg-black h-screen overflow-hidden flex justify-center items-center w-full tracking-tighter text-white">
+      <ShaderGradientCanvas
+        importedFiber={{ ...fiber, ...drei, ...reactSpring }}
+        style={{
+          position: "absolute",
+          top: 0,
+          pointerEvents: "none",
+          height: "100vh",
+        }}
+      >
+        <ShaderGradient
+          control="query"
+          urlString="https://www.shadergradient.co/customize?animate=on&axesHelper=on&bgColor1=%23000000&bgColor2=%23000000&brightness=0&cAzimuthAngle=180&cDistance=3.9&cPolarAngle=115&cameraZoom=1&color1=%230d00ff&color2=%23000000&color3=%23000000&destination=onCanvas&embedMode=off&envPreset=city&format=gif&fov=45&frameRate=10&grain=off&lightType=3d&pixelDensity=1&positionX=-0.5&positionY=0.1&positionZ=0&range=enabled&rangeEnd=40&rangeStart=0&reflection=1&rotationX=0&rotationY=0&rotationZ=235&shader=defaults&type=waterPlane&uAmplitude=0&uDensity=1.1&uFrequency=5.5&uSpeed=0.1&uStrength=2.4&uTime=0.2&wireframe=false&zoomOut=false"
+        />
+      </ShaderGradientCanvas>
       <div className="fixed top-0 left-0 p-4 flex items-end w-full flex-col gap-2">
         <div className="flex gap-2">
           <a href="https://instagram.com/joaquinmetayer" target="_blank">
