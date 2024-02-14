f103843bcdde854a1a67b222d3f211fae0f79dc9
Joaquin Metayer <joaquinmetayer@gmail.com>
2024-02-14 10:11:19 -0300

    30

--- src/app/56bluefx/page.tsx
+++ src/app/56bluefx/page.tsx
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 export default function Home() {
   useEffect(() => {
     window.location.href =
-      "https://linktw.in/eAgpsC";
+      "https://linktw.in/STmPBj";
   }, []);
   return (
     <div className="bg-black h-screen overflow-hidden flex justify-center items-center w-full tracking-tighter text-white">
