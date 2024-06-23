a4e0c67e512bdcdd33bcf80c389632350bc0ef33
joaquinmetayer <joaquinmetayer@gmail.com>
2024-06-22 21:43:59 -0300

    hide email on admin

--- pages/admin/components/AdminPanel.tsx
+++ pages/admin/components/AdminPanel.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 
 const AdminPanel = () => {
+  //<li>
+  //  <Link href="/admin/send-email">Send email</Link>
+  //</li>
   return (
     <>
       <ul>
@@ -11,13 +14,10 @@ const AdminPanel = () => {
           <Link href="/admin/delete-post">Delete post</Link>
         </li>
         <li>
-          <Link href="/admin/edit-links">Edit links</Link>
-        </li>
-        <li>
-          <Link href="/admin/send-email">Send email</Link>
-        </li>
-        <li>
           <Link href="/admin/change-image">Hero image</Link>
+        </li>
+        <li>
+          <Link href="/admin/edit-links">Edit links</Link>
         </li>
       </ul>
     </>
