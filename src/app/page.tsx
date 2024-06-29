14170cac1faf57f27b3ce231de50a5ab1fc8e1c9
joaquinmetayer <joaquinmetayer@gmail.com>
2024-06-29 11:28:06 -0300

    subscribers component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import getPostMetadata from "../../components/getPostMetadata";
 import PostPreview from "../../components/PostPreview";
 import data from "../../assets/data.json";
+import Subscribers from "./components/Subscribers";
 
 export default function Home() {
   const postMetadataPublic = getPostMetadata("posts");
@@ -17,21 +18,29 @@ export default function Home() {
       {data.description.length > 0 && (
         <>
           <p>{data.description}</p>
-          <hr />
+          <p></p>
         </>
       )}
 
-      {postMetadataPublic.length === 0 && (// cambiar a !== para que se vea
+      {postMetadataPublic.length === 0 && ( // cambiar a !== para que se vea
         <>
-          <p><b>Destacados</b></p>
+          <hr />
+          <p>
+            <b>Destacados</b>
+          </p>
           <hr />
           {postPreviewsPinned}
-          <hr />
         </>
       )}
-
-      <p><b>{postMetadataPublic.length} posts</b></p>
       <hr />
+
+      <p>
+        <b>
+          {postMetadataPublic.length} posts
+        </b>
+      </p>
+      <hr />
+
       {postPreviewsPublic}
     </>
   );
