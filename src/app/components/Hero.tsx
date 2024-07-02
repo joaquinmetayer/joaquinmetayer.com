21f65523a185f0c1b3ceb814f14af4f0ad533f81
joaquin.metayer <joaquin.metayer@bitlogic.io>
2024-07-02 12:03:34 -0300

    links

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -4,9 +4,16 @@ import Link from "next/link";
 export default function Hero() {
   return (
     <>
-      <h1>
+      <h2>
         <Link href="/">joaquinmetayer</Link>
-      </h1>
+      </h2>
+      <p>
+        <a href="https://linkedin.com/in/joaquinmetayer/" target="_blank">LinkedIn</a>{' '}
+        <a href="https://github.com/joaquinmetayer" target="_blank">GitHub</a>{' '}
+        <a href="https://youtube.com/channel/UChepHxgZmukOteW-6c796wQ" target="_blank">YouTube</a>{' '}
+        <a href="https://instagram.com/joaquinmetayer" target="_blank">Instagram</a>{' '}
+      </p>
+      <hr />
     </>
   );
 }
